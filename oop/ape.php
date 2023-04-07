3befaf551a0c1db2a024aa7d9a4d7da11235577f
riskisian <riskisian214@gmail.com>
2023-04-07 15:35:55 +0700

    class ape Riski Sianturi

---
+++ oop/ape.php
@@ -0,0 +1,14 @@
+<?php 
+
+    require_once('animal.php');
+    class Ape extends Animal{
+        public $legs = 2;
+
+        public function Yell(){
+            echo "Yell : Auooo";
+        }
+    }
+
+
+
+?>
